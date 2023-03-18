339dc8d552eabaaa21533ec183598e1364ce2069
wilson <wilsonmiguel2003@gmail.com>
2023-03-18 17:53:41 -0300

    cambios en practica 1

--- Practica 1/app.js
+++ Practica 1/app.js
@@ -1,21 +1,32 @@
 const app = document.getElementById("app")
 
+const h = React.createElement;
+
 const Avatar = params => {
-    const src = `https://randomuser.me/api/portraits/women/${params.id}.jpg`
+    const src = `https://randomuser.me/api/portraits/women/${params.id}.jpg`;
 
-    return `
-    <picture>
-    <img src="${src}">
-    <em>${params.name}</em>
-    </picture>
-    `
-}
+    return h('img', {src})
+};
 
-app.innerHTML += Avatar ({ id: 5, name: "Andrea" })
-app.innerHTML += Avatar ({ id: 3, name: "Paula" })
+ReactDOM.render(h(Avatar, {id: 1}), app)
 
-app.querySelectorAll("img").forEach(img => {
-    img.addEventListener("click", () => {
-        img.classList.toggle("disabled")
-    })
-})
+
+// const Avatar = params => {
+//     const src = `https://randomuser.me/api/portraits/women/${params.id}.jpg`
+
+//     return `
+//     <picture>
+//     <img src="${src}">
+//     <em>${params.name}</em>
+//     </picture>
+//     `
+// }
+
+// app.innerHTML += Avatar ({ id: 5, name: "Andrea" })
+// app.innerHTML += Avatar ({ id: 3, name: "Paula" })
+
+// app.querySelectorAll("img").forEach(img => {
+//     img.addEventListener("click", () => {
+//         img.classList.toggle("disabled")
+//     })
+// })
